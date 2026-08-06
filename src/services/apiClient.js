a685945d85b7c10034e48ada99388b8bc04c2681
Purush-5268 <23pa1a04b4@vishnu.edu.in>
2026-08-06 11:02:51 +0530

    Changed apiclient.js to empty quotes

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -7,7 +7,7 @@ import {
 } from "../utils/tokenStorage";
 import { API_ENDPOINTS } from "../constants/apiEndpoints";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "";
+const BASE_URL = "";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
